e48317adcae8048709a88d8641078029b092efc9
Berkaycan Sakar <berkaycansakar@gmail.com>
2023-07-08 17:05:45 +0300

    Service Extensions added

---
+++ WebAPI/Extensions/ServicesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.EfCore;
+
+namespace WebAPI.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void AddConfigureSqlContext(this IServiceCollection services,
+            IConfiguration configuration) =>
+             services.AddDbContext<RepositoryContext>(options =>
+                  options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        
+    }
+}
