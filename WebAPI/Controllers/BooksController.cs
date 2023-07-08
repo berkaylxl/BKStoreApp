60644d911c6b1da6130631850f691b252e6233b2
Berkaycan Sakar <berkaycansakar@gmail.com>
2023-07-08 14:21:50 +0300

    Add project files.

---
+++ WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Repository;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name ="id")] int id)
+        {
+            var book  = _context.Books
+                .Where(x => x.Id == id)
+                .SingleOrDefault();
+
+             return book == null ? NotFound() : Ok(book);
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _context.Books.ToList();
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+           if(book is null) { return BadRequest(); }
+            _context.Books.Add(book);
+            _context.SaveChanges();
+            return StatusCode(201, book);
+        }
+    }
+}
---
+++ WebAPI/Repository/RepositoryContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+using WebAPI.Repository.Config;
+
+namespace WebAPI.Repository
+{
+    public class RepositoryContext:DbContext
+    {
+
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+        public DbSet<Book> Books { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig());
+        }
+    }   
+}
